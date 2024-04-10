d39d226a493121e1ed45435508db59e387450bd5
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2024-04-10 15:50:27 +0200

    Add example for new_contactmatrix_list()

--- R/contactmatrix_list.R
+++ R/contactmatrix_list.R
@@ -1,3 +1,25 @@
+#' Constructor for a `contactmatrix_list` object
+#'
+#' @param ... `contactmatrix` objects to include in the list. All elements
+#' must have the same groupings.
+#'
+#' @returns
+#' A list of `contactmatrix` objects, of class `contactmatrix_list`.
+#'
+#' @examples
+#' cm1 <- new_contactmatrix(
+#'   from  = c("00_05", "05_10", "05_10"),
+#'   to    = c("00_05", "10_15", "15_20"),
+#'   value = c(0.32   , 0.46   , 0.72   )
+#' )
+#'
+#' cm2 <- new_contactmatrix(
+#'   from  = c("05_10", "05_10", "05_10"),
+#'   to    = c("00_05", "10_15", "15_20"),
+#'   value = c(0.27   , 0.09   , 0.32   )
+#' )
+#'
+#' cml <- new_contactmatrix_list(cm1, cm2)
 new_contactmatrix_list <- function(...) {
 
   res <- list(...)
