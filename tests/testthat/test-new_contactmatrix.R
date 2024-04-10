b92f35112cc85f0f49471990a658c01975b069eb
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2024-04-10 12:11:38 +0200

    Add first basic test file

---
+++ tests/testthat/test-new_contactmatrix.R
@@ -0,0 +1,34 @@
+test_that("new_contactmatrix() generates correct class and format", {
+  cm2d <- new_contactmatrix(
+    from  = c("00_05", "05_10", "05_10"),
+    to    = c("00_05", "10_15", "15_20"),
+    value = c(0.32   , 0.46   , 0.72   )
+  )
+
+  expect_s3_class(cm2d, "contactmatrix")
+  # matrix class is conserved for 2d arrays
+  expect_s3_class(cm2d, "matrix")
+  expect_s3_class(cm2d, "array")
+  expect_identical(unname(dim(cm2d)), c(4L, 4L))
+
+  # Multiple groupings; gender & case
+  dat <- data.frame(
+    age    = c("young", "young", "old"),
+    gender = c("male", "female", "female"),
+    age    = c("old", "old", "young"),
+    gender = c("female", "female", "female"),
+    value  = c(1, 2, 2)
+  )
+
+  cm3d <- new_contactmatrix(
+    from  = dat[, c(1, 2)],
+    to    = dat[, c(3, 4)],
+    value = dat[, "value"]
+  )
+
+  expect_s3_class(cm3d, "contactmatrix")
+  expect_s3_class(cm3d, "array")
+  expect_identical(unname(dim(cm3d)), c(2L, 2L, 2L, 2L))
+  expect_match(names(dimnames(cm3d)), "(from|to)$")
+
+})
