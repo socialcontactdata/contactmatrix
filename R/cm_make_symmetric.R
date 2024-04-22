423348f7613c05acd712e2594a10bb03e8b8a88d
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2024-04-22 20:25:00 +0200

    Create cm_make_symmetric()

---
+++ R/cm_make_symmetric.R
@@ -0,0 +1,26 @@
+#' Make a `contactmatrix` object symmetric
+#'
+#' Symmetricity for social contact matrices is defined as
+#' $c_{ij}N_i == c_{ji}N_j$ where c is the contact value and N the group size.
+#'
+#' @param x A `contactmatrix` object to make symmetric
+#' @param population A numeric vector of the population size of each group
+#'
+#' @return A symmetric `contactmatrix` object
+#'
+#' @export
+cm_make_symmetric <- function(x, population) {
+
+  # TODO: add some input checking
+
+  if (attr(x, "symmetric")) {
+    return(x)
+  }
+
+  popx <- population * x
+  res <- (popx + t(popx)) / 2 / population
+
+  attr(res, "symmetric") <- TRUE
+  return(res)
+
+}
