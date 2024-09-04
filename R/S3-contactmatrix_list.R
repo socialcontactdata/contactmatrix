55a579f69fed64957526262e955ace9677cf7a52
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2024-09-04 20:31:14 +0200

    Create tidy methods

--- R/S3-contactmatrix_list.R
+++ R/S3-contactmatrix_list.R
@@ -83,3 +83,39 @@ cm_get_groupings.contactmatrix_list <- function(x, ...) {
   return(attr(x[[1]], "groupings"))
 
 }
+
+#' Convert a `contactmatrix_list` to a tidy `data.frame`
+#'
+#' @param x A `contactmatrix_list` object
+#' @param ... Ignored for now
+#'
+#' @importFrom generics tidy
+#'
+#' @export
+#'
+#' @examples
+#' cm1 <- new_contactmatrix(
+#'   from  = c("[0,5)", "[5,10)",  "[5,10)"),
+#'   to    = c("[0,5)", "[10,15)", "[15,20)"),
+#'   value = c(0.32   , 0.46   , 0.72   )
+#' )
+#'
+#' cm2 <- new_contactmatrix(
+#'   from  = c("[5,10)",  "[0,5)",   "[5,10)"),
+#'   to    = c("[15,20)", "[10,15)", "[10,15)"),
+#'   value = c(0.27   , 0.09   , 0.32   )
+#' )
+#'
+#' cml <- new_contactmatrix_list(cm1, cm2)
+#'
+#' tidy(cml)
+tidy.contactmatrix_list <- function(x, ...) {
+
+  res <- lapply(seq_along(x), function(i) {
+    out <- tidy(x[[i]])
+    out$index <- i
+    return(out)
+  })
+
+  do.call(rbind.data.frame, res)
+}
